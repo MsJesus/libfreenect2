b5daca29cce42f98fac5636b8307fbcb08db6aa5
Aheyeu <siarhei.aheyeu.88@gmail.com>
2017-08-23 03:07:00 +0300

    - disable opengl

--- src/openni2/DeviceDriver.cpp
+++ src/openni2/DeviceDriver.cpp
@@ -435,6 +435,8 @@ namespace Freenect2Driver
         std::string serial = freenect2.getDeviceSerialNumber(i);
 
         std::string uri = devid_to_uri(i) + "?serial=" + serial;
+        register_uri(uri);
+/*
         const char* modes_c[] = {
           "",
           "&depth-size=640x480",
@@ -448,6 +450,7 @@ namespace Freenect2Driver
         for (int j = 0; j < 3; j++) {
           register_uri(uri + modes[j]);
         }
+*/
 
 #if 0
         freenect_device* dev;
